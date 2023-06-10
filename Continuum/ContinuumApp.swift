a8f40ff102cfaf990dcdb4bce764bfd4e81c6874
Jack W <jackweatherilt@outlook.com>
2023-06-10 21:38:06 +0100

    Initial Commit

---
+++ Continuum/ContinuumApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContinuumApp.swift
+//  Continuum
+//
+//  Created by Jack on 10/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ContinuumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
