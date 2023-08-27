c64064ce4c996573ab677286946b8567358dbe34
Jack W <jackweatherilt@outlook.com>
2023-08-27 23:25:51 +0100

    Setup Watch app.

---
+++ WatchContinuum Watch App/WatchContinuumApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchContinuumApp.swift
+//  WatchContinuum Watch App
+//
+//  Created by Jack on 24/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct WatchContinuum_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
